2ad1e6e6ae8acfbf7da297f7243d21e1dececdba
anster01 <ah1219@imperial.ac.uk>
2020-11-25 12:46:18 +0000

    Started testing bashfile

--- test/test_mips_cpu_harvard.sh
+++ test/test_mips_cpu_harvard.sh
@@ -1,1 +1,18 @@
-#!bin/bash
+#!/bin/bash
+set -eou pipefail
+
+SRC="$1"
+#check if source directory is valid
+if test -f "${SRC}/mips_cpu_harvard.v" ; then
+    #check how many parameters passed
+    if [ $# -eq 1 ]; then
+        >&2 echo "Testing all instructions"
+    elif [ $# -eq 2 ]; then
+        INSTR="$2"
+        >&2 echo "Testing ${INSTR}"
+    else
+        >&2 echo "Incorrect number of arguments"
+    fi
+else
+    >&2 echo "Invalid source directory"
+fi
