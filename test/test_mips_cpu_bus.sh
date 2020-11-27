78ae707c085f8a7f454060152a8f29a7115513b5
anster01 <ah1219@imperial.ac.uk>
2020-11-27 16:37:13 +0000

    testfiles for bus

--- test/test_mips_cpu_bus.sh
+++ test/test_mips_cpu_bus.sh
@@ -1,1 +1,20 @@
-#!bin/bash
+#!/bin/bash
+set -eou pipefail
+
+SRC="$1"
+#check if source directory is valid
+if test -f "${SRC}/mips_cpu_bus.v" ; then
+    #check how many input passed
+    if [ $# -eq 1 ]; then
+        #if only source directory provided, test all instructions
+        ./test/test_all_instructions_bus.sh ${SRC}
+    elif [ $# -eq 2 ]; then
+        #if instruction given test only that inustruction
+        INSTR="$2"
+        ./test/test_one_instruction_bus.sh ${SRC} ${INSTR}
+    else
+        >&2 echo "Incorrect number of arguments"
+    fi
+else
+    >&2 echo "Invalid source directory"
+fi
