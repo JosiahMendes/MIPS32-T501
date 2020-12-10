f906fc49c5e219a6d48bbd97b4d21056a5fe2cbf
kc319 <kc319@ic.ac.uk>
2020-12-10 21:41:30 +0000

    bgezal2 test case + update

--- test/testcases/bgezal/bgezal1.asm
+++ test/testcases/bgezal/bgezal1.asm
@@ -5,4 +5,4 @@ jr $0
 addiu $2 $2 0x1
 addiu $2 $2 0x11
 jr $31
-sll $0 $0 0x0
+addiu $2 $2 0x1
---
+++ test/testcases/bgezal/bgezal2.asm
@@ -0,0 +1,9 @@
+addiu $8 $0 0xF123
+addiu $2 $0 0x1
+bgezal $8 0x4
+sll $0 $0 0x0
+jr $0
+addiu $2 $2 0x1
+addiu $2 $2 0x11
+jr $31
+addiu $2 $2 0x1
