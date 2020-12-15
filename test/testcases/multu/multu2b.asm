90a6c0011f8f1476f4c75c5a768f25b27cc35c47
ubuntu <zd819@ic.ac.uk>
2020-12-15 21:51:36 +0000

    Pushing to Main

--- test/testcases/multu/multu2b.asm
+++ test/testcases/multu/multu2b.asm
@@ -1,14 +1,5 @@
-<<<<<<< HEAD:test/testcases/multu/multu2.asm
-addiu v0 r0 0x2
-addiu v1 r0 0xffff
-mult v0 v1
-mfhi v0
-jr r0
-mflo v0
-=======
 addiu v0 r0 0x2
 addiu v1 r0 0xFFFF
 multu v0 v1
 jr r0
 mfhi v0
->>>>>>> a81eb0b1ea658d4a99ff829acb8712e0adac98a1:test/testcases/multu/multu2b.asm
