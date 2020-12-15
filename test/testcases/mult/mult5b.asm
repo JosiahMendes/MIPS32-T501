e6543973ce6dc5e787cdf413df533d9a62c80f9f
Matei <root@Matei.localdomain>
2020-12-15 21:05:22 +0000

    MULT tests working

--- test/testcases/mult/mult5b.asm
+++ test/testcases/mult/mult5b.asm
@@ -1,8 +1,10 @@
-lui v0 0x7FFF
-lui v1 0x7FFF
-addiu t2 r0 0xffff
-addiu t3 r0 0xffff
+lui v0 0xffff
+srl v0 v0 16
+lui t2 0x7fff
 addu v0 v0 t2
+lui v1 0xffff
+srl v1 v1 16
+lui t3 0x7fff
 addu v1 v1 t3
 mult v0 v1
 jr r0
--- test/testcases/mult/mult6b.asm
+++ test/testcases/mult/mult6b.asm
@@ -1,6 +1,6 @@
 lui v0 0x8000
 addiu t3 r0 0x1
-sub v0 v0 t3
+subu v0 v0 t3
 lui v1 0x8000
 mult v0 v1
 jr r0
