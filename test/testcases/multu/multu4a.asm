c0ec2cc53d9d1a9c8d63f937c4c61fa262aed55f
ubuntu <zd819@ic.ac.uk>
2020-12-15 22:32:42 +0000

    Fixed multu testcasses

--- test/testcases/multu/multu4a.asm
+++ test/testcases/multu/multu4a.asm
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-addiu v0 r0 0x0
-addiu v1 r0 0x0
-multu v0 v1
-mfhi v0
-=======
 addiu v0 r0 0xffff
 addiu v1 r0 0xffff
 multu v0 v1
->>>>>>> a81eb0b1ea658d4a99ff829acb8712e0adac98a1
 jr r0
 mflo v0
