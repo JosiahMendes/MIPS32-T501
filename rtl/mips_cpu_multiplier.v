6e06e2a3564d7c4ff47217661f3b135fb6d5e817
Josiah Mendes <josiahmendes@icloud.com>
2020-12-18 17:37:14 +0000

    Wrapped multiplier in another unit to prevent logic duplication

--- rtl/mips_cpu_multiplier.v
+++ rtl/mips_cpu_multiplier.v
@@ -1,46 +1,19 @@
-module mips_cpu_multiplier(
+module mips_cpu_multiplier
+    (
     input logic [31:0] a, //Using signed logic for now, until a problem with arises
     input logic [31:0] b,//Coudlnt use signed ports, had a port decleration error
-    input logic sign, clk,
+    input logic  clk, sign,
     output logic [63:0] out
-);
-    always_ff @(negedge clk) begin
+    );
 
-        if(sign)begin out = $signed(a)*$signed(b); end
-        else begin out =$unsigned(a)*$unsigned(b); end
-    end
-/*
-       logic [1:0] temp;
-       integer i;
-       logic E1;
-       logic [31:0] b1;
-       always @ (a, b)
-       begin
-         out = 64'd0;
-         E1 = 1'd0;
-         for (i = 0; i < 32; i = i + 1)
-         begin
-           temp = {a[i], E1};
-           b1 = - b;
-             case (temp)
-               2'd2 : out [63 : 32] = out [63 : 32] + b1;
-               2'd1 : out [63 : 32] = out [63 : 32] + b;
-               default : begin end
-             endcase
-               out = out >> 1;
+    wire [31:0] Multiplicand,Multiplier;
+    wire [63:0] Product;
+    assign Multiplicand = (sign) ? (a[31]) ? -a :a :a;
+    assign Multiplier = (sign) ? (b[31]) ? -b :b :b;
+    assign out = (sign) ? (b[31]==a[31]) ? Product : -Product : Product;
 
-               out[63] = out[62];
+    mips_cpu_multiplieru inst(
+        .clk(clk), .a(Multiplicand), .b(Multiplier), .out(Product)
+    );
 
-
-               E1 = a[i];
-                   end
-
-            //if(b[31]==1)
-               if (b == 32'd2147483648)
-               begin
-                 out = - out;
-          end
-
-        end
-*/
 endmodule
---
+++ rtl/mips_cpu_multiplieru.v
@@ -0,0 +1,44 @@
+module mips_cpu_multiplieru(
+    input logic [31:0] a, //Using signed logic for now, until a problem with arises
+    input logic [31:0] b,//Coudlnt use signed ports, had a port decleration error
+    input logic  clk,
+    output logic [63:0] out
+);
+    always_ff @(negedge clk) begin
+        out <= $unsigned(a)*$unsigned(b);
+    end
+/*
+       logic [1:0] temp;
+       integer i;
+       logic E1;
+       logic [31:0] b1;
+       always @ (a, b)
+       begin
+         out = 64'd0;
+         E1 = 1'd0;
+         for (i = 0; i < 32; i = i + 1)
+         begin
+           temp = {a[i], E1};
+           b1 = - b;
+             case (temp)
+               2'd2 : out [63 : 32] = out [63 : 32] + b1;
+               2'd1 : out [63 : 32] = out [63 : 32] + b;
+               default : begin end
+             endcase
+               out = out >> 1;
+
+               out[63] = out[62];
+
+
+               E1 = a[i];
+                   end
+
+            //if(b[31]==1)
+               if (b == 32'd2147483648)
+               begin
+                 out = - out;
+          end
+
+        end
+*/
+endmodule
